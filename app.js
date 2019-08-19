6d0484631088aa27d18cbe95f983a4b6d0191168
Dong Mei Lim <52555301+dongmeilim@users.noreply.github.com>
2019-08-19 15:32:55 +1200

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const port = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
+  let dataFile = require('./smym-data.json');
+  console.log(dataFile.data[0].Summary);
   res.end('<h1>What is up?</h1></h2>HHHHHHHIIII</h2>');
 });
 
